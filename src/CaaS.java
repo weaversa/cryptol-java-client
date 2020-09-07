3984480885e53ade2279e8b8902cf289c8473286
Sean Weaver <weaversa@gmail.com>
2020-09-07 01:33:21 +0000

    Added Send, Receive, and LoadModule commands

--- src/CaaS.java
+++ src/CaaS.java
@@ -19,8 +19,11 @@ public class CaaS {
 	    socket = new Socket(ip_address, port);
 	    out    = new PrintStream(socket.getOutputStream());
 	    in     = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	    state  = null;
+	    state  = new JSONObject();
+	    state.put("state", JSONObject.NULL);
+	    
             id     = new JSONObject();
+	    id.put("id", 0);
 	    
 	    return true;
 	}
@@ -36,8 +39,63 @@ public class CaaS {
 
     public boolean Send(JSONObject message) {
 	//todo
+	message.putOnce("jsonrpc", "2.0");
+
+	JSONObject params = message.optJSONObject("params");
 	
+	params.put("state", state.opt("state"));
+	message.put("id", id.optInt("id"));
+
+	String netstring = message.toString();
+	int netstring_size = netstring.length();
+
+        netstring = Integer.toString(netstring_size) + ":" + netstring + ",";
+
+	System.out.println("Sending " + netstring);
+	
+	out.print(netstring);
+
 	return true;
+    }
+
+    public String Receive() {
+	String result = "";
+	try{
+	    int c, i;
+	    String length = "";
+	    while((c=in.read())!=':'){
+		length = length + ((char) c);
+	    }
+	    System.out.print(length + "\n");
+	    for(i = 0; i < Integer.parseInt(length); i++) {
+		c = in.read();
+		result = result + ((char) c);
+	    }
+	    c = in.read();
+	    if(c != ',') {
+		//Something went wrong
+	    }
+
+	} catch (Exception e) {
+	    e.printStackTrace();	    
+	}
+
+	return result;
+    }
+    
+    public void LoadModule(String module_name) {
+	JSONObject message = new JSONObject();
+
+	message.put("method", "load module");
+
+	JSONObject moduleName = new JSONObject();
+	moduleName.put("module name", module_name);
+
+	message.put("params", moduleName);
+
+	//reset state?
+
+	Send(message);
     }
     
     //For testing
@@ -45,5 +103,10 @@ public class CaaS {
 	CaaS caas = new CaaS();
 	boolean success = caas.Connect(args[0], Integer.parseInt(args[1]));
 	System.out.println("Success: " + success);
+
+	caas.LoadModule("Primitive::Symmetric::Cipher::Block::AES");
+
+	System.out.println(caas.Receive());
+	
     }
 }
