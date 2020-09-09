305ce501ac64a8314f0b27c4606d3a4da3daadbd
Sean Weaver <weaversa@gmail.com>
2020-09-09 20:05:27 +0000

    Added methods for parsing out sequences of hex strings from results. Saved the state in a stack.

--- src/CaaS.java
+++ src/CaaS.java
@@ -2,15 +2,22 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Stack;
 import org.json.*;
 
 public class CaaS {
     private Socket socket;
     private PrintStream out;
     private BufferedReader in;
-
-    private String state;
+    private boolean connected;
+    
+    Stack<String> states;
     private int id;
+
+    CaaS () {
+	connected = false;
+	states = new Stack<String>();
+    }
     
     //If the connection is successful, return true, otherwise return
     //false
@@ -19,42 +26,52 @@ public class CaaS {
 	    socket = new Socket(ip_address, port);
 	    out    = new PrintStream(socket.getOutputStream());
 	    in     = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-	    state  = null;
-            id     = 0;
 
-	    JSONObject result = LoadModule("Cryptol");
+	    connected = true;
 	    
-	    return true;
+   	    //Empty the states stack
+	    while(!states.empty()) {
+		states.pop();
+	    }
+	    states.push(null);
+	    id     = 0;
+
+    	    JSONObject result = LoadModule("Cryptol");
 	}
 	catch (Exception e) {
 	    e.printStackTrace();
-	    return false;
+	    connected = false;
 	}
+
+	return connected;
     }
 
     public void Disconnect() {
+	connected = false;
+
 	//todo
     }
 
     public void ResetState() {
-	state = null;
-	
-	//Load the prelude (we could instead save the prelude
-	//state directly after loading the prelude in `Connect` and
-	//assign that value to `state` here.
-	JSONObject result = LoadModule("Cryptol");
+	while(states.size() > 2) {
+	    states.pop();
+	}
+
+	//This will either do nothing, or pop until the state is
+	//the one directly after loading the prelude in `Connect`.
     }
     
     private boolean Send(JSONObject message) {
-	//todo
+	if(!connected) return false;
+	
 	message.putOnce("jsonrpc", "2.0");
 
 	JSONObject params = message.getJSONObject("params");
 
-	if(state == null) {
+	if(states.peek() == null) {
 	    params.put("state", JSONObject.NULL);
 	} else {
-	    params.put("state", state);
+	    params.put("state", states.peek());
 	}
 	
 	message.put("id", id);
@@ -65,8 +82,13 @@ public class CaaS {
         netstring = Integer.toString(netstring_size) + ":" + netstring + ",";
 
 	System.out.println("Sending: " + message.toString());
-	
-	out.print(netstring);
+
+	try {
+	    out.print(netstring);
+	} catch (Exception e) {
+	    System.out.println("Send failed: " + e);
+	    return false;
+	}
 
 	return true;
     }
@@ -109,7 +131,7 @@ public class CaaS {
 
 	//Pull out new state and save
 	JSONObject r = resultJSON.getJSONObject("result");
-	state = r.getString("state");
+	states.push(r.getString("state"));
 
 	System.out.println("Result: " + r.toString());
 	
@@ -209,7 +231,7 @@ public class CaaS {
 	try {
 	    expression = bv.getString("expression");
 	} catch (JSONException e) {
-	    System.out.println("Missing 'expression' tag");
+	    System.out.println("Problem with 'expression' tag: " + e.toString());
 	    return null;
 	}
 	if(!expression.equals("bits")) {
@@ -220,7 +242,7 @@ public class CaaS {
 	try {
 	    encoding = bv.getString("encoding");
 	} catch (JSONException e) {
-	    System.out.println("Missing 'encoding' tag");
+	    System.out.println("Problem with 'encoding' tag: " + e.toString());
 	    return null;
 	}
 	if(!encoding.equals("hex")) {
@@ -231,23 +253,54 @@ public class CaaS {
 	try {
 	    data = bv.getString("data");
 	} catch (JSONException e) {
-	    System.out.println("Missing 'data' tag");
+	    System.out.println("Problem with 'data' tag: " + e.toString());
 	    return null;
 	}
 
-	System.out.println(bv.toString());
-	
 	//Get width
 	try {
 	    width = bv.getInt("width");
 	} catch (JSONException e) {
-	    System.out.println("Missing 'width' tag");
+	    System.out.println("Problem with 'width' tag: " + e.toString());
 	    return null;
 	}
 
 	return data;	
     }
-    
+
+    public String[] GetHexArray(JSONObject bv) {
+	String expression;
+	JSONArray data;
+	int width;
+
+	//Check for correct expression tag
+	try {
+	    expression = bv.getString("expression");
+	} catch (JSONException e) {
+	    System.out.println("Problem with 'expression' tag: " + e.toString());
+	    return null;
+	}
+	if(!expression.equals("sequence")) {
+	    System.out.println("'expression' tag not \"sequence\"");
+	}    
+
+	//Get data
+	try {
+	    data = bv.getJSONArray("data");
+	} catch (JSONException e) {
+	    System.out.println("Problem with 'data' tag: " + e.toString());
+	    return null;
+	}
+
+	String[] ret = new String[data.length()];
+
+	for(int i = 0; i < data.length(); i++) {
+	    ret[i] = GetHex(data.getJSONObject(i));
+	}
+
+	return ret;	
+    }
+	
     //For testing
     public static void main(String[] args) {
 	CaaS caas = new CaaS();
@@ -264,9 +317,30 @@ public class CaaS {
 	caas.LoadModule("Primitive::Symmetric::Cipher::Block::AES");
 	JSONObject aesResult =
 	    caas.EvaluateExpression("aesEncrypt(10, 11)");
-
 	String ct = caas.GetHex(aesResult.getJSONObject("answer").getJSONObject("value"));
 
 	System.out.println("ct = 0x" + ct);
+
+	//Test state reset
+	caas.ResetState();
+
+	hexResult =
+	    caas.EvaluateExpression("0xab10");
+
+	String[] hexArrayResult =
+	    caas.GetHexArray(
+	        caas.EvaluateExpression(
+                    caas.FromHexArray(new String[]{"ab", "bc", "de"}, 8)).getJSONObject("answer").getJSONObject("value"));
+
+	for(int i = 0; i < hexArrayResult.length; i++) {
+	    System.out.print(hexArrayResult[i] + " ");
+	}
+	System.out.println();
+	
+	caas.LoadModule("Primitive::Symmetric::Cipher::Block::AES");
+	aesResult =
+	    caas.EvaluateExpression("aesEncrypt(10, 11)");
+	ct = caas.GetHex(aesResult.getJSONObject("answer").getJSONObject("value"));
+	
     }
 }
