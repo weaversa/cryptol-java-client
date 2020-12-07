a86914ca6c66ee0d24dc685a4c759efdc3ff852e
Frank <auricratio@mac.com>
2020-12-07 07:42:05 -0500

    a little code to display using Cryptol sequences

---
+++ test/CryptolSequenceTest.java
@@ -0,0 +1,16 @@
+import na.na.na.cryptol.BinaryString;
+import na.na.na.cryptol.CryptolValue;
+import na.na.na.cryptol.caas.CaaSException;
+import na.na.na.cryptol.caas.ModuleSpecificCaaS;
+
+public class CryptolSequenceTest {
+  
+  public static void main(String[] args) throws CaaSException {
+    BinaryString a0 = BinaryString.valueOf("10010");
+    BinaryString a1 = BinaryString.valueOf("01011");
+        
+    CryptolValue c = new CryptolValue(new BinaryString[] {a0, a1});
+    
+  }
+  
+}
